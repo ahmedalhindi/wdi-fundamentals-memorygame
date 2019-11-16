a2b69a51fbab219083210f7c380d6e61f3bc0ea6
Ahmed AlHindi <Alhindi.ahmed@gmail.com>
2019-11-16 16:40:35 +0300

    Used functions to organize js code

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,20 +1,26 @@
 const cards = ["king","king","queen","queen"];
 const cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne)
-
-console.log("User flipped "+cardOne);
-
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped "+cardTwo);
-
-if (cardsInPlay.length===2){
-	if (cardsInPlay[0]===cardsInPlay[1]){
-	console.log("You found a match")}
-	else if (cardsInPlay[0]!==cardsInPlay[1]){
-		console.log("Sorry, try again")
+function checkForMatch(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	  console.log("You found a match!");
+	} else {
+	  console.log("Sorry, try again.");
 	}
 }
+function flipCard(cardId){
+
+	console.log("User flipped "+ cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+
+	if (cardsInPlay.length===2){
+		checkForMatch();
+	}
+}
+
+flipCard(0);
+flipCard(2);
+
+
+
+
